953a5e97123b975e3561e5e79400a984cb063712
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2024-07-15 21:05:05 -0300

    projeto collections java

---
+++ collections-java/src/ManipulcaoString.java
@@ -0,0 +1,53 @@
+class ManipulcaoString {
+    public static void main(String[] args) {
+
+   /*o metodo valueOf() converte diferentes tipos de valores em string.
+    double variavel1 = 13.5;
+    String convertida = String.valueOf(variavel1);
+        System.out.println(convertida);*/
+
+       /* O método charAt() da classe Java String retorna um valor char no número de índice fornecido
+       String variavel2 = "ricardo";
+        System.out.println(variavel2.charAt(0));
+        int comprimento = variavel2.length();
+        System.out.println(variavel2.charAt(comprimento -6));*/
+
+        /*
+        String variavel3 = "ricardo";
+        System.out.println(variavel3.startsWith("r",4));
+        System.out.println(variavel3.endsWith("o"));*/
+
+        /* metodo length() retorna a quantidade de caracteres que a String tem
+        metodo trim() remove todos os espaços do inicio e do fim a String
+        String variavel4 = "  ricardo  ";
+        System.out.println(variavel4.length());
+        String variavelNova = variavel4.trim();
+        System.out.println(variavelNova.length()); */
+
+        /* metodo toUpperCase() deixa a String toda com letras maiusculas
+           metodo tolowerCase() deixa a String toda com letras minusculas
+        String variavel5 = "ricardo";
+        String variavelg = variavel5.toUpperCase();
+        System.out.println(variavelg);
+        System.out.println(variavelg.toLowerCase()); */
+
+        /* os métodos indexOf e lastIndexOf em Java. Esses métodos são usados
+         para buscar a primeira ou última ocorrência de um caractere ou trecho
+         de string dentro de outra string. Eles retornam o índice da ocorrência ou -1 caso não encontrem.
+        String variavel6 = "ricardo";
+        System.out.println(variavel6.indexOf("r",1));
+        System.out.println(variavel6.lastIndexOf("o"));
+         */
+
+        /*O método replace() da classe String do Java retorna uma string substituindo todos os char ou CharSequence antigos pelos novos char ou CharSequence.
+        O método substring() da classe Java String retorna uma parte da string.
+        String variavel7 = "ricardo";
+        System.out.println(variavel7.replace("r","d"));
+        System.out.println(variavel7.substring(1));
+        */
+
+
+
+    }
+
+    }
